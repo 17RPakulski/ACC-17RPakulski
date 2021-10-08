14b9693d6ce11eae25d92232c49264555290c910
17RPakulski <92167234+17RPakulski@users.noreply.github.com>
2021-10-08 14:58:00 +0100

    Add files via upload

---
+++ cs 8.10.21 worksheet.py
@@ -0,0 +1,45 @@
+
+firstName = input('type your first name: ')
+surname = input('type your surname: ')
+print('welcome', firstName, surname)
+
+age = int(input('please type your age: '))
+print('welcome', firstName, surname, age + 1)
+
+height = float(input('what is your height: '))
+
+print('welcome', firstName, surname, age + 1, height)
+
+num1 = input('first number: ')
+num2 = input('second number: ')
+print(int(num1) + int(num2))
+ 
+num3 = input('first number: ')
+num4 = input('second number: ')
+print(int(num3) - int(num4))
+
+num5 = input('first number: ')
+num6 = input('second number: ')
+print(int(num5) * int(num6))
+
+num7 = input('first number: ')
+num8 = input('second number: ')
+print(int(num7) / int(num8))
+
+num9 = input('first number: ')
+num10 = input('second number: ')
+print(int(num9) // int(num10))
+
+num11 = input('first number: ')
+num12 = input('second number: ')
+print(int(num11) // int(num12))
+
+num11 = input('first number: ')
+num12 = input('second number: ')
+print(int(num11) % int(num12))
+
+num13 = input('first number: ')
+num14 = input('second number: ')
+num15 = input('third number: ')
+print((int(num13) + int(num14) + int(num15)) / 3)
+
