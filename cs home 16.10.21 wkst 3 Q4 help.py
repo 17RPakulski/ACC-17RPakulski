986d885d1e8fd7fe05ca7133dd926c102487acb7
17RPakulski <92167234+17RPakulski@users.noreply.github.com>
2021-10-16 12:00:05 +0100

    Add files via upload

---
+++ cs home 16.10.21 wkst 3 Q4 help.py
@@ -0,0 +1,38 @@
+# ask teacher wkst 3 q4
+'''
+counter = 0
+factoral = 0
+user = 0
+total = 0
+
+user = int(input('Enter Number: '))
+
+while counter < factoral:
+    
+    factoral = factoral + 1
+    
+    counter = counter + 1
+    
+    total = user * factoral
+    
+print(total)
+'''
+
+# third time trying
+# ASK TEACHER
+
+num = int(input('Enter Number: '))
+factoral = num
+counter = 0
+total = 0
+
+while counter < factoral:
+    counter = counter + 1
+    total = total + num * factoral
+    factoral = factoral - 1
+    
+print(total)
+
+
+    
+    
